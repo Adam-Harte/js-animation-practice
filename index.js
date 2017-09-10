354c86d84d577815304cf60b9edb12307a7dc0da
Adam-Harte <adamharte11@hotmail.co.uk>
2017-09-10 18:58:55 +0100

    added transiiton and keyframe css animations

---
+++ index.js
@@ -0,0 +1,9 @@
+var colorBtn = document.getElementById('color');
+colorBtn.onclick = function(){
+    this.style.backgroundColor = 'red';
+};
+var growingBtn = document.getElementById('growing');
+growingBtn.onclick = function(){
+    this.style.fontSize = '36px';
+    this.style.color = 'red';
+};
